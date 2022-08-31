1a2c8157ad190b0753cc37c281297cd138b86c4b
Dushko Avramoski <Dushko.Avramoski@netcetera.com>
2022-08-31 17:16:49 +0200

    added deployed backend server

--- src/axios/axiosInstance.js
+++ src/axios/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://powerbi-edu.azurewebsites.net/api",
     headers: {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
